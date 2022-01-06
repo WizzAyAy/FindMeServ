07b409b255e3225d49d3572d90e01c51ef3efbe6
QuentinMaignan <quentin.maignan@ignimission.com>
2022-01-06 14:09:40 +0100

    filter

--- Django/settings.py
+++ Django/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -25,8 +24,7 @@ SECRET_KEY = 'django-insecure-4*%_)f77p#cr89#)!z(!u+-bgc70b5^im2ajb0ud7#n9_i-512
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
 
 # Application definition
 
@@ -72,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Django.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -82,7 +79,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -102,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -113,7 +108,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
--- FindMeServ/views.py
+++ FindMeServ/views.py
@@ -8,6 +8,10 @@ import logging
 logger = logging.getLogger('FindMeServ views')
 
 
+def home(request):
+    return render(request, '../templates/base.html')
+
+
 def add_server_form(request):
     types = Server.ServerType.choices
     context = {'types': types}
@@ -37,32 +41,44 @@ def add_server(request):
         logging.error(traceback.format_exc())
         add_server_statement = (False, 'Impossible to add the server')
 
-    types = Server.ServerType.choices
-
     logger.error(type(add_server_statement))
-    context = {'types': types, 'add_server_statement': add_server_statement}
+    context = {'types': Server.ServerType.choices, 'add_server_statement': add_server_statement}
     return render(request, '../templates/addServer.html', context)
 
 
-
 def server_list(request):
+    empty = request.POST.get('empty')
+    host = request.POST.get('host')
+    gamemode = request.POST.get('gamemode')
+
     servers = Server.objects.all()
+
+    logger.error(host)
+    if host != "":
+        servers = servers.filter(host=host)
+
+    logger.error(gamemode)
+    if gamemode != "None":
+        servers = servers.filter(gamemode=gamemode)
+
     servers_to_send = []
     for server in servers:
         info = get_server_info(server)
-        servers_to_send.append({
-            'ip': server.get_ip(),
-            'port': server.get_port(),
-            'map': info.map_name,
-            'player': info.player_count,
-            'max_player': info.max_players,
-            'gamemode': server.get_gamemode_display(),
-            'host': server.get_host(),
-            'name': info.server_name,
-        })
+        if (info.player_count != 0 and empty is None) or empty == 'on':
+            servers_to_send.append({
+                'ip': server.get_ip(),
+                'port': server.get_port(),
+                'map': info.map_name,
+                'player': info.player_count,
+                'max_player': info.max_players,
+                'gamemode': server.get_gamemode_display(),
+                'host': server.get_host(),
+                'name': info.server_name,
+            })
 
     logger.error(servers_to_send)
-    context = {'servers': servers_to_send}
+    context = {'servers': servers_to_send, 'types': Server.ServerType.choices, 'empty': empty, 'host': host,
+               'gamemode': gamemode}
     return render(request, '../templates/serverList.html', context)
 
 
