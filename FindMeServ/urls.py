3c1a9005830f0cfc6cdcf2bcc69533491ba50ca7
WizzAyAy <quentindu4913@hotmail.fr>
2022-01-09 16:10:57 +0100

    temporary disable add servers

--- FindMeServ/urls.py
+++ FindMeServ/urls.py
@@ -7,9 +7,9 @@ app_name = 'FindMeServ'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('server-list/', views.server_list, name='serverList'),
+    # path('server-list/', views.server_list, name='serverList'),
     path('add-server/', views.add_server, name='addServer'),
-    path('add-server-form/', views.add_server_form, name='addServerForm'),
+    # path('add-server-form/', views.add_server_form, name='addServerForm'),
     path('get-players-info/', views.get_players_info, name='getPlayersInfo'),
 ]
 
